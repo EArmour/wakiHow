53b86e23ecb76cc5a38d76a6681d1d53e80462f9
Evan Armour <hargrimm@gmail.com>
2015-06-01 19:11:46 -0400

    Add API functionality using flask_restful

--- wakiHow/__init__.py
+++ wakiHow/__init__.py
@@ -1,10 +1,54 @@
-from flask import Flask, render_template, request, g
-import random, sqlite3
+# -*- coding: utf-8 -*-
+from flask import Flask, Markup, render_template, request, g
+from flask_restful import Resource, Api, reqparse, abort
+import random, re, sqlite3
 
 DB = 'static/wh.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+api = Api(app)
+
+class step_api(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('count', type=int, default=3, help='Number of steps to retrieve')
+        parser.add_argument('profit', type=bool, default=False, help='Develop a profit-seeking plan')
+        args = parser.parse_args(strict=True)
+        steps = args['count']
+        if steps > 1000:
+            abort(400, message="Cannot retrieve more than 1000 steps per request.")
+        c = g.db.execute("SELECT boldtext FROM steps ORDER BY RANDOM() LIMIT %d" % steps)
+        rows = c.fetchall()
+        steps = {}
+        for i, step in enumerate(rows):
+            steps[i+1] = Markup(step[0]).striptags()
+
+        if args['profit']:
+            steps[len(steps)+1] = u'???'
+            steps[len(steps)+1] = u'Profit!'
+
+        return steps
+
+class image_api(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('count', type=int, default=3, help='Number of image URLs to retrieve')
+        args = parser.parse_args(strict=True)
+        count = args['count']
+        if count > 1000:
+            abort(400, message="Cannot retrieve more than 1000 image URLs per request.")
+        c = g.db.execute("SELECT html FROM images ORDER BY RANDOM() LIMIT %d" % count)
+        rows = c.fetchall()
+        images = {}
+        for i, html in enumerate(rows):
+            src = re.search(r'src="(.*?)"', html[0])
+            images[i + 1] = src.group(1)
+
+        return images
+
+api.add_resource(step_api, '/api/steps')
+api.add_resource(image_api, '/api/images')
 
 @app.before_request
 def before_request():
@@ -38,18 +82,18 @@ def display():
 
 def select_steps(num):
     c = g.db.execute("SELECT boldtext, imgid, source FROM steps ORDER BY RANDOM() LIMIT %d" % num)
-    rows= c.fetchall()
+    rows = c.fetchall()
 
     steps, sourceids = [], []
     for i, row in enumerate(rows):
         imgid = g.db.execute("SELECT html FROM images WHERE id=?", (row[1],))
         html = imgid.fetchone()
-        if imgid:
+        if html:
             assembled = '<li>' + html[0].decode('utf-8')
         else:
             assembled = '<li>'
         assembled += '<div class="step_num">%d</div>' % (i + 1) + row[0] + '</li>'
-        steps.append(assembled)
+        steps.append(Markup(assembled))
         sourceids.append(row[2])
     return [steps, sourceids]
 
@@ -57,7 +101,7 @@ def select_rand(table, field, num=1):
     count = random.randint(0, num)
     c = g.db.execute("SELECT %s FROM %s ORDER BY RANDOM() LIMIT %d" % (field, table, count))
     rows = c.fetchall()
-    return [row[0].decode('utf-8') for row in rows]
+    return [Markup(row[0]) for row in rows]
 
 def select_sources(ids):
     sources = []
@@ -65,9 +109,9 @@ def select_sources(ids):
     for sourceid in ids:
         c = g.db.execute("SELECT url, title FROM urls WHERE id=?", (sourceid,))
         src = c.fetchone()
-        sources.append('<li><a href="%s">%s</a></li>' % (src[0], src[1]))
+        sources.append(Markup('<li><a href="%s">%s</a></li>' % (src[0], src[1])))
 
     return sources
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', use_reloader=True, debug=False, port=80)
